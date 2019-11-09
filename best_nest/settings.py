72a08616dfa81bda5186f2ca04d415cc1a881034
Jeff Salisbury <salisbury.jeffery@gmail.com>
2019-11-08 21:56:38 -0500

    UPdatea

--- best_nest/settings.py
+++ best_nest/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = '/home/ec2-user/best-nest-api/static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
